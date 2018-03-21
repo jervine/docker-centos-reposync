e579884413a92f45853d1d703988419f8a2d6aa9
Jonny Ervine <jervine@users.noreply.github.com>
2018-03-21 10:16:55 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,13 +6,16 @@
 ## Initialise any variables being called:
 # Set the correct timezone
 TZ=${TZ:-UTC}
-CONF_DIR=${CONF_DIR:/config/confs/}
+CONF_DIR=${CONF_DIR:-/config/confs/}
 setup=/config/.setup
 
 if [ ! -f "${setup}" ]; then
   rm -f /etc/localtime
   cp /usr/share/zoneinfo/$TZ /etc/localtime
   echo "IncludeOptional $CONF_DIR/*.conf" >> /etc/httpd/conf/httpd.conf
+  if [ -f "/config/bin/setup-ansible-user.sh" ]; then
+    /config/bin/setup-ansible-user.sh
+  fi
   touch $setup
 fi
 
